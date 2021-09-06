b9dbc7bfff83cebb89f84076e090bd153779e3e6
Hai Nguyen H.P <hai.nguyen3@monstar-lab.com>
2021-09-06 09:10:29 +0700

    Lazy load

--- bai1/my-app/src/App.js
+++ bai1/my-app/src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Home from './pages/home/Home';
-import ProductList from "./pages/products/Products";
-import Account from "./pages/account/Account";
-import Login from "./pages/login/Login";
-import ProductDetail from "./pages/products/ProductDetail";
-import PrivateRouter from "./components/private/PrivateRouter";
+// import Home from './components/footer/Footer';
+// import ProductList from "./pages/products/Products";
+// import Account from "./pages/account/Account";
+// import Login from "./pages/login/Login";
+// import ProductDetail from "./pages/products/ProductDetail";
+// import PrivateRouter from "./components/private/PrivateRouter";
 
+const Home = React.lazy(() => import('./pages/home/Home'));
+const ProductList = React.lazy(() => import('./pages/products/Products'));
+const Account = React.lazy(() => import('./pages/account/Account'));
+const Login = React.lazy(() => import('./pages/login/Login'));
+const ProductDetail = React.lazy(() => import('./pages/products/ProductDetail'));
+const PrivateRouter = React.lazy(() => import('./components/private/PrivateRouter'));
 
 function App() {
   return (
     <Router>
       <Header/>
         <div className="container">
+          <Suspense fallback={<span>Loading...</span>}>
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route path="/products" component={ProductList}/>
@@ -23,6 +30,7 @@ function App() {
                 <Route path="/login" component={Login}/>
                 <Route path="/product/:id" component={ProductDetail}/>
             </Switch>
+            </Suspense>
         </div>
       <Footer/>
     </Router>
