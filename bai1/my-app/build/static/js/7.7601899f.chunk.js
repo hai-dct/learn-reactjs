73b2c3b8d86324dd9a8c9f1bfccb0cc5d1b654b5
Hai Nguyen H.P <hai.nguyen3@monstar-lab.com>
2021-09-06 09:25:04 +0700

    deploy product

---
+++ bai1/my-app/build/static/js/7.7601899f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[7],{32:function(e,s,a){"use strict";a.r(s);a(0);var c=a(1);s.default=function(){return Object(c.jsx)("div",{className:"page",children:Object(c.jsx)("h2",{children:"Home Page"})})}}}]);
+//# sourceMappingURL=7.7601899f.chunk.js.map
